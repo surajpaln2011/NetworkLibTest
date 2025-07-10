c21f395210bbc12b62567bd7641cb7558fdd723b
Suraj Pal <surajpaln2011@gmail.com>
2025-07-10 19:00:50 +0530

    Fix: remove circular dependency and prepare networklib for JitPack

--- networklib/build.gradle.kts
+++ networklib/build.gradle.kts
@@ -33,15 +33,14 @@ android {
 }
 
 dependencies {
-    implementation(project(":app"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.okhttp.logging.interceptor)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
